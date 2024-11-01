234354e5bc656c4b4cc8ec221cb1d11a1f502fdd
Basti <basti.babaeva@yandex.ru>
2024-11-01 21:47:42 +0700

    создание функции для сохранения и добавления нового элемента
    переименования кнопок на русский язык

--- script 8.java
+++ script 8.java
@@ -2,24 +2,37 @@ let elements = [];
 
 function createNewElement()
 {
-    const container = document.querySelector('.element-container');
+    const container = document.querySelector('.elements-container');
     const newElement = document.createElement('div');
     newElement.className='element';
 
-    newElement.innerHTML='
+    newElement.innerHTML=`
         <label>
             <input type="text">
         </label>
         <label>
             <input type="number">
         </label>
-        <button onclick="upElement(this.parentNode)" value="up">up</button>
-        <button onclick="downElement(this.parentNode)" value="down">down</button>
-        <button onclick="deleteElement(this.parentNode)" value="delete">delete</button>
-        ';
+        <button onclick="upElement(this.parentNode)" value="вверх">up</button>
+        <button onclick="downElement(this.parentNode)" value="вниз">down</button>
+        <button onclick="deleteElement(this.parentNode)" value="удалить">delete</button>
+        `;
         container.appendChild(newElement);
 
 }
+
+function printResults() {
+    const result = {};
+    document.querySelectorAll('.element').forEach((element) => {
+        const label = element.querySelector('input[type="text"]').value;
+        const number = element.querySelector('input[type="number"]').value;
+        result[`${label}`] = `${number}`;
+    });
+
+    const resultOutput = document.querySelector('.result-output');
+    resultOutput.textContent = JSON.stringify(result);
+}
+
 
 function upElement(element){
     const prevElement = element.previousElementSibling;
